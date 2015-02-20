d1605c50740062ab46eb41e39de1928743f946d9
Marcus Tenório <marcusvtenorio@gmail.com>
2015-02-20 00:24:03 -0200

    Added red ball when an empty square is clicked

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,20 +1,23 @@
 var MAZE = [[0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-            [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+            [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
             [1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1],
             [1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1],
             [1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1],
             [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
             [1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1],
             [1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1],
             [1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1],
             [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1],
             [1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1]];
 
 var MazeEmptySquare = React.createClass({
+  handleClick: function() {
+    this.props.onEmptySquareClick({key: this.props.squareKey});
+  },
   render: function() {
     return (
-      <div className="empty-square col-xs-1"></div>
+      <div className="empty-square col-xs-1" onClick={this.handleClick}>{this.props.children.toString()}</div>
     );
   }
 });
@@ -28,27 +31,33 @@ var MazeFullSquare = React.createClass({
 });
 
 var MazeBoard = React.createClass({
+  handleEmptySquareClick: function(square) {
+    console.log(square);
+    this.setState({selectedSquare: square.key});
+  },
   getInitialState: function() {
     return {
-      startingPosition: '11-3'
+      selectedSquare: '11-3'
     };
   },
   render: function() {
-
     var board = [];
 
     for (var i = 0; i < MAZE.length; i++) {
       for (var j = 0; j < MAZE[i].length; j++) {
+        var key = i + "-" + j;
         if (MAZE[i][j]) {
-          board.push(<MazeFullSquare />);
+          board.push(<MazeFullSquare key={key} />);
         } else {
-          board.push(<MazeEmptySquare key={i + "-" + j} />);
+          var content = this.state.selectedSquare == key ? '⚫' : '';
+          board.push(<MazeEmptySquare key={key} squareKey={key} onEmptySquareClick={this.handleEmptySquareClick}>{content}</MazeEmptySquare>);
         }
-      };
-    };
+      }
+    }
 
     return (
-      <div className="row">{board}
+      <div className="row">
+        {board}
       </div>
     );
   }
